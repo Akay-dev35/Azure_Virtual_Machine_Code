34ba1f26decb51fd4d66da3a9848b77cf3ee8054
Amit Kumar <142219533+Akay-dev35@users.noreply.github.com>
2026-07-14 10:28:35 +0530

    Define variables for Azure subscription and environment
    Added new variables for Azure deployment configuration.

--- tfrepo/variables.tf
+++ tfrepo/variables.tf
@@ -1,1 +1,92 @@
-variable "virtual_machines"{type=map(any)}
+variable "subscription_id" {
+
+  type        = string
+  description = "Azure Subscription ID"
+
+}
+
+variable "environment" {
+
+  type        = string
+  description = "Deployment Environment"
+
+  default = "dev"
+
+}
+
+variable "project_name" {
+
+  type = string
+
+}
+
+variable "owner" {
+
+  type = string
+
+}
+
+variable "cost_center" {
+
+  type = string
+
+}
+
+variable "virtual_machines" {
+
+  description = "Collection of Azure Virtual Machines"
+
+  type = map(object({
+
+    name = string
+
+    location = string
+
+    resource_group_name = string
+
+    vm_size = optional(string)
+
+    admin_username = string
+
+    admin_password = optional(string)
+
+    network_interface_ids = list(string)
+
+    image = object({
+
+      publisher = string
+      offer      = string
+      sku        = string
+      version    = string
+
+    })
+
+    os_disk = object({
+
+      caching = string
+
+      storage_account_type = string
+
+      disk_size_gb = optional(number)
+
+    })
+
+    zone = optional(string)
+
+    tags = optional(map(string), {})
+
+    identity = optional(object({
+
+      type = string
+
+    }))
+
+    boot_diagnostics = optional(object({
+
+      storage_account_uri = string
+
+    }))
+
+  }))
+
+}
