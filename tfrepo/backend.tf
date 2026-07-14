c7c2d55ddc2bf33216280ac1af37e1b0b10231df
Amit Kumar <142219533+Akay-dev35@users.noreply.github.com>
2026-07-14 10:27:02 +0530

    Add Terraform backend configuration for Azure

--- tfrepo/backend.tf
+++ tfrepo/backend.tf
@@ -1,1 +1,12 @@
-# backend
+terraform {
+
+  backend "azurerm" {
+
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                  = ""
+
+  }
+
+}
