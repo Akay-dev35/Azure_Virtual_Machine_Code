6195809349b8c7a1400284a45fecc913a7bc3b61
Amit Kumar <142219533+Akay-dev35@users.noreply.github.com>
2026-07-14 10:27:29 +0530

    Add locals configuration with common tags and default VM size

--- tfrepo/locals.tf
+++ tfrepo/locals.tf
@@ -1,1 +1,15 @@
-locals{}
+locals {
+
+  common_tags = {
+
+    ManagedBy  = "Terraform"
+    Environment = var.environment
+    Project     = var.project_name
+    Owner       = var.owner
+    CostCenter  = var.cost_center
+
+  }
+
+  default_vm_size = "Standard_B2s"
+
+}
