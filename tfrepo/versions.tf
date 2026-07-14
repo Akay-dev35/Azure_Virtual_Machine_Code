8f8a3f2e3125757273e563335557a81e7807d803
Amit Kumar <142219533+Akay-dev35@users.noreply.github.com>
2026-07-14 10:26:06 +0530

    Update Terraform configuration with required version and providers

--- tfrepo/versions.tf
+++ tfrepo/versions.tf
@@ -1,1 +1,14 @@
-terraform{}
+terraform {
+
+  required_version = ">= 1.6.0"
+
+  required_providers {
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+
+  }
+
+}
